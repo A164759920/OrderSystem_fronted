4fc786e255c3e37405d0e5ef9285c72051ef3ac5
A164759920 <1405527125@qq.com>
2022-12-15 20:58:22 +0800

    feat:统计图

--- src/components/AnalyseData.vue
+++ src/components/AnalyseData.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="container">
         <div class="top-sale">
-            <div class="chart-title">
-                菜 品 销 量
-            </div>
+            <div class="chart-title">菜 品 销 量</div>
             <div id="saleChart"></div>
         </div>
         <div class="seven-count">
@@ -22,56 +20,112 @@ export default {
     data: function () {
         return {
             domain: BUS.DOMAIN,
-            typeSaleXData: [],
-            typeSaleYData: []
+            typeSale_X_Data: [],
+            typeSale_Y_Data: [],
+            sumCount_X_Data: [],
+            sumCount_Y_Data: [],
+        }
+    },
+    methods: {
+        initCountChart: function () {
+            (async function (ctx) {
+                try {
+                    const chartData = await axios.get(`${ctx.domain}/ordersumcount`)
+                    if (chartData.data.code === 0) {
+                        chartData.data.data.forEach(item => {
+                            ctx.sumCount_X_Data.push(item.Odate)
+                            ctx.sumCount_Y_Data.push(item.sum)
+                        })
+                        var countChart = echarts.init(document.getElementById("countChart"))
+                        var option = {
+                            xAxis: {
+                                type: 'category',
+                                data: ctx.sumCount_X_Data,
+                                name: "日期",
+                                axisLabel: {
+                                    align: "center"
+                                }
+                            },
+                            yAxis: {
+                                type: 'value',
+                                name: "营业额(元)",
+                                axisLabel: {
+                                    align: 'center'
+                                }
+                            },
+                            series: [
+                                {
+                                    data: ctx.sumCount_Y_Data,
+                                    type: 'line'
+                                }
+                            ]
+                        };
+                        countChart.setOption(option)
+                    } else {
+                        ctx.$notify.error({
+                            title: chartDate.data.msg,
+                            msg: chartDate.data.error
+                        })
+                    }
+                } catch (error) {
+                    ctx.$notify.error({
+                        title: "图表②渲染失败",
+                        msg: error
+                    })
+                }
+            })(this)
+        },
+        initSaleChart: function () {
+            (async function (ctx) {
+                try {
+                    const chartData = await axios.get(`${ctx.domain}/DishTypeSales`)
+                    if (chartData.data.code === 0) {
+                        // 设置X和Y轴的数据
+                        chartData.data.data.forEach(item => {
+                            ctx.typeSale_X_Data.push(item.Dname)
+                            ctx.typeSale_Y_Data.push(item.count)
+                        })
+                        // 渲染数据至图表
+                        var saleChart = echarts.init(document.getElementById("saleChart"))
+                        var option = {
+                            tooltip: {},
+                            legend: {
+                                data: ['销量']
+                            },
+                            xAxis: {
+                                data: ctx.typeSale_X_Data
+                            },
+                            yAxis: {},
+                            series: [
+                                {
+                                    name: '销量',
+                                    type: 'bar',
+                                    data: ctx.typeSale_Y_Data
+                                }
+                            ]
+                        };
+                        saleChart.setOption(option)
+                    }
+                    else {
+                        ctx.$notify.error({
+                            title: chartData.data.msg,
+                            msg: chartData.data.error
+                        })
+                    }
+                } catch (error) {
+                    ctx.$notify.error({
+                        title: "图表①渲染失败",
+                        msg: error
+                    })
+                }
+            })(this)
         }
     },
     mounted: function () {
-        // 渲染 top-sale图表
-        (async function (ctx) {
-            try {
-                const chartData = await axios.get(`${ctx.domain}/DishTypeSales`)
-                if (chartData.data.code === 0) {
-                    // 设置X和Y轴的数据
-                    chartData.data.data.forEach(item => {
-                        ctx.typeSaleXData.push(item.Dname)
-                        ctx.typeSaleYData.push(item.count)
-                    })
-                    // 渲染数据至图表
-                    var saleChart = echarts.init(document.getElementById("saleChart"))
-                    var option = {
-                        tooltip: {},
-                        legend: {
-                            data: ['销量']
-                        },
-                        xAxis: {
-                            data: ctx.typeSaleXData
-                        },
-                        yAxis: {},
-                        series: [
-                            {
-                                name: '销量',
-                                type: 'bar',
-                                data: ctx.typeSaleYData
-                            }
-                        ]
-                    };
-                    saleChart.setOption(option)
-                }
-                else {
-                    ctx.$notify.error({
-                        title: chartData.data.msg,
-                        msg: chartData.data.error
-                    })
-                }
-            } catch (error) {
-                ctx.$notify.error({
-                    title: "图表渲染失败",
-                    msg: error
-                })
-            }
-        })(this)
-
+        // 渲染 top-sale 条形图表
+        this.initSaleChart()
+        // 渲染 sevent-count 折线图表
+        this.initCountChart()
     }
 }
 </script>
